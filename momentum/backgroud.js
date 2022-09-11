2f1b55bb02647c5407e39866ad794c373ac8d5bb
moodawnee <107897834+moodawnee@users.noreply.github.com>
2022-09-12 05:59:55 +0900

    Delete momentum directory

--- momentum/backgroud.js
+++
@@ -1,8 +0,0 @@
-const img = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg",];
-
-const chosenImg = img[Math.floor(Math.random()*img.length)];
-
-const bgImg = document.createElement("img");
-bgImg.src = `img/${chosenImg}`;
-
-document.body.appendChild(bgImg);
